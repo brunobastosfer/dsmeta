5a297eb7ac4744e867a5fc5adcfaf5b64fafe8c0
Bruno Bastos Fernandes <bruno.bafernandes@b2wdigital.com>
2022-07-14 20:25:41 -0300

    other components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,19 @@
 import NotificationButton from "./components/NotificationButton";
+import Header from "./components/Header";
+import SalesCard from "./components/SalesCard";
 
 function App() {
   return (
-    <div>
-      <h1>Olá mundo!</h1>
-      <NotificationButton />
-    </div>
+    <>
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard />
+          </div>
+        </section>
+      </main>
+    </>
   );
 }
 
